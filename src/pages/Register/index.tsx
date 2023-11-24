a2c5ec98e74c78d9351c15ba0fe06b51edf303d3
JP-Conde5 <126258606+JP-Conde5@users.noreply.github.com>
2023-11-24 04:11:42 -0300

    Update index.tsx

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,3 +1,4 @@
+//FormData = Guarda informações de formulário
 import { FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaKey } from "react-icons/fa";
